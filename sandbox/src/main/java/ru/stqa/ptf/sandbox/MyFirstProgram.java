d1f39169c2f8a72c97224f9b3c910d161bfe0207
sms-spec <smsspec@yandex.ru>
2020-02-06 18:13:13 +0300

    методы функции, объекты

--- sandbox/src/main/java/ru/stqa/ptf/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox/MyFirstProgram.java
@@ -2,7 +2,24 @@ package ru.stqa.ptf.sandbox;
 
 public class MyFirstProgram {
 
-   public static void main(String[] args) {
-   System.out.println("Hello, world!");
-}
+  public static void main(String[] args) {
+    hello (" world");
+    hello (" user");
+    hello (" Liliia");
+
+    Square s = new Square (5);
+    System.out.println("Площадь квадрата со стороной " + s.l + "=" + s.area());
+
+    Rectangle r = new Rectangle (4,6);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + "=" + r.area());
+  }
+
+  public static void hello(String sambody) {
+    System.out.println("Hello," + sambody + " !");
+  }
+
+  public static double area(Square s) {
+    return s.l * s.l;
+  }
+
 }
